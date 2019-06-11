8020117c97e1c0d5bf3a82f161437d32dc583873
Dragan Diana-Andreea <33541782+DDianaAndreea@users.noreply.github.com>
2019-06-11 22:21:14 +0300

    update emails

--- bloodbank.sql
+++ bloodbank.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gazdă: 127.0.0.1
--- Timp de generare: mai 27, 2019 la 06:13 PM
+-- Timp de generare: iun. 11, 2019 la 09:19 PM
 -- Versiune server: 10.1.39-MariaDB
 -- Versiune PHP: 7.3.5
 
@@ -44,8 +44,8 @@ CREATE TABLE `campanii` (
 --
 
 INSERT INTO `campanii` (`ID`, `denumire`, `descriere`, `data`, `locatie`, `imagine`, `in_asteptare`, `participanti`) VALUES
-(6, 'Spitalul Judetean Slatina', 'doneaza impreuna cu alti oameni la centrul de transfuzii', '2019-08-20', 'Centrul BoodBank', 'drops.jpg', 1, 0),
-(7, 'Drop of life', 'Impreuna putem ajuta.\r\nAlatura-te la o noua capanie de donare de sange ce riveste toti pacientii nostri.', '2019-08-20', 'Slatina', 'drops-of-life.jpg', 0, 0);
+(6, 'Drop of life', 'Impreuna putem ajuta.\r\nAlatura-te la o noua capanie de donare de sange ce priveste toti pacientii nostri.', '2019-08-20', 'Centrul BoodBank', 'drops.jpg', 1, 0),
+(7, 'Drop of life', 'Impreuna putem ajuta.\r\nAlatura-te la o noua capanie de donare de sange ce priveste toti pacientii nostri.', '2019-08-20', 'Slatina', 'drops-of-life.jpg', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -55,7 +55,7 @@ INSERT INTO `campanii` (`ID`, `denumire`, `descriere`, `data`, `locatie`, `imagi
 
 CREATE TABLE `carnetul_donatorului` (
   `id` int(11) NOT NULL,
-  `cnp` int(13) NOT NULL,
+  `cnp` varchar(13) NOT NULL,
   `seria` varchar(2) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `numarul` int(6) NOT NULL,
   `localitate` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -69,10 +69,9 @@ CREATE TABLE `carnetul_donatorului` (
 --
 
 INSERT INTO `carnetul_donatorului` (`id`, `cnp`, `seria`, `numarul`, `localitate`, `judet`, `adresa`, `data_ultimei_donari`) VALUES
-(1, 2147483647, 'OT', 677288, 'Slatina', 'Olt', 'Aleea viorelelor, GA5A, et.8, ap.34', '2019-05-27 13:15:05'),
-(2, 2147483647, 'OT', 677288, 'Slatina', 'Olt', 'Aleea Rozelor, FB23, sc.C, et2, ap.5', '2019-05-09 12:59:23'),
-(6, 2147483647, 'OT', 677288, 'Slatina', 'Olt', 'Aleea viorelelor, GA5A, et.8, ap.34', '2019-05-09 14:38:24'),
-(7, 2147483647, 'OT', 552674, 'Slatina', 'Olt', 'Aleea Rozelor, FB23, sc.C, et2, ap.5', '2019-05-12 15:47:44');
+(1, '2960820284541', 'OT', 677288, 'Slatina', 'Olt', 'Aleea viorelelor, GA5A, et.8, ap.34', '2019-06-11 17:49:06'),
+(2, '1960221284569', 'OT', 676842, 'Slatina', 'Olt', 'Aleea Rozelor, FB23, sc.C, et2, ap.5', '2019-06-09 10:53:07'),
+(8, '2760605284374', 'OT', 619101, 'Slatina', 'Olt', 'Aleea viorelelor, GA5A, et.8, ap.34', '2019-06-11 17:56:03');
 
 -- --------------------------------------------------------
 
@@ -93,7 +92,7 @@ CREATE TABLE `cazuri_publicate` (
 --
 
 INSERT INTO `cazuri_publicate` (`ID`, `nume`, `prenume`, `grupa_sanguina`, `caz`) VALUES
-(1, 'Diana-Andreea', 'Dragan', 'AB', 'adfzsgxdhcvb');
+(1, 'Diana-Andreea', 'Dragan', 'AB', 'Aplazie medulară. Până în momentul operației trebuie să se facă transfuzii sanguine zilnice cu grupa AB pozitiv.');
 
 -- --------------------------------------------------------
 
@@ -107,7 +106,7 @@ CREATE TABLE `donatori` (
   `nume` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `data_nastere` date NOT NULL,
-  `telefon` int(10) NOT NULL,
+  `telefon` varchar(10) NOT NULL,
   `sex` varchar(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `rh` varchar(1) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `grupa_sanguina` varchar(2) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
@@ -147,11 +146,15 @@ CREATE TABLE `donatori` (
 --
 
 INSERT INTO `donatori` (`ID`, `prenume`, `nume`, `email`, `data_nastere`, `telefon`, `sex`, `rh`, `grupa_sanguina`, `c1`, `c2`, `c3`, `c4`, `c5`, `c6`, `c7`, `c8`, `c9`, `c10`, `c11`, `c12`, `c13`, `c14`, `c15`, `c16`, `c17`, `c18`, `c19`, `c20`, `c21`, `c22`, `c23`, `c24`, `c25`, `c26`, `c27`, `activ`, `data_cererii`) VALUES
-(1, 'Diana-Andreea', 'Dragan', 'diana@gmail.com', '1996-08-20', 760373781, 'f', '+', 'AB', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 2, '2019-04-06 12:09:28'),
-(2, 'Octavian', 'Iancu', 'octavian@gmail.com', '1994-02-21', 770506027, 'm', '+', 'B', 'Da', 'Nu', 'Nu', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 3, '2019-04-06 12:27:39'),
-(5, 'Diana', 'Dragan', 'dyusk_96@yahoo.com', '1996-08-20', 760373781, 'f', '+', 'AB', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 0, '2019-05-09 17:35:14'),
-(6, 'Diana', 'Dragan', 'dianadragan@yahoo.com', '1996-08-20', 760373781, 'f', '+', 'A', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 3, '2019-05-09 17:36:04'),
-(7, 'Jeffrey', 'Iancu', 'jeffrey_iancu@yahoo.com', '1994-02-21', 766107379, 'm', '-', 'AB', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 2, '2019-05-11 17:01:03');
+(1, 'Diana-Andreea', 'Dragan', 'dyusk_96@yahoo.com', '1996-08-20', '760373781', 'f', '+', 'AB', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 2, '2019-04-06 12:09:28'),
+(2, 'Octavian', 'Iancu', 'octavian@gmail.com', '1994-02-21', '770506027', 'm', '-', 'A', 'Da', 'Nu', 'Nu', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 1, '2019-04-06 12:27:39'),
+(8, 'Elena', 'Dragan', 'dyusk_96@yahoo.com', '1976-06-05', '0766107379', 'f', '+', 'B', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 2, '2019-06-09 13:44:58'),
+(9, 'Ionel', 'Dragan', 'ionel_dragan@gmail.com', '1972-05-06', '0760373781', 'm', '+', 'A', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 0, '2019-06-09 13:46:30'),
+(11, 'Diana', 'Dragan', 'dyusk_96@yahoo.com', '1996-08-20', '0760373781', 'f', '+', 'AB', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 0, '2019-06-10 12:26:49'),
+(12, 'Diana', 'Dragan', 'dyusk_96@yahoo.com', '1996-08-20', '0760373781', 'f', '+', 'AB', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 0, '2019-06-10 12:37:32'),
+(13, 'Diana', 'Dragan', 'dyusk_96@yahoo.com', '1996-08-20', '0760373781', 'f', '+', 'AB', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 0, '2019-06-10 12:39:37'),
+(14, 'Diana', 'Dragan', 'dyusk_96@yahoo.com', '1996-08-20', '0760373781', 'f', '+', 'AB', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 0, '2019-06-10 12:39:52'),
+(15, 'Diana', 'Dragan', 'dyusk_96@yahoo.com', '1996-08-20', '0760373781', 'f', '+', 'AB', 'Da', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 'Nu', 0, '2019-06-10 12:41:20');
 
 -- --------------------------------------------------------
 
@@ -165,20 +168,19 @@ CREATE TABLE `mesaje` (
   `prenume` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `mesaj` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_turkish_ci NOT NULL,
-  `stare_email` int(11) DEFAULT NULL
+  `stare_email` int(11) DEFAULT NULL,
+  `data` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Eliminarea datelor din tabel `mesaje`
 --
 
-INSERT INTO `mesaje` (`ID`, `nume`, `prenume`, `mesaj`, `email`, `stare_email`) VALUES
-(1, 'Diana-Andreea', 'Andreea', 'eyrfg', 'diana@gmail.com', 0),
-(2, 'Iancu', 'Andreea', 'etyrhdtfgj', 'dyusk_96@yahoo.com', 0),
-(3, 'Dragan', 'Andreea', 'Hello', 'diana@yahoo.com', 0),
-(4, 'Dragan', 'Diana', 'footer', 'dyusk_96@yahoo.com', 0),
-(5, 'Dragan', 'Diana', 'contact', 'dyusk_96@yahoo.com', 0),
-(6, 'Dragan', 'Diana', 'proba', 'dyusk_96@yahoo.com', 1);
+INSERT INTO `mesaje` (`ID`, `nume`, `prenume`, `mesaj`, `email`, `stare_email`, `data`) VALUES
+(1, 'Diana-Andreea', 'Andreea', 'eyrfg', 'dyusk_96@yahoo.com', 1, '2019-06-12 10:00:00'),
+(2, 'Iancu', 'Andreea', 'etyrhdtfgj', 'dyusk_96@yahoo.com', 0, '2019-06-09 00:00:00'),
+(3, 'Dragan', 'Andreea', 'Hello', 'diana@yahoo.com', 0, '2019-06-19 00:00:00'),
+(4, 'Dragan', 'Diana', 'footer', 'dyusk_96@yahoo.com', 1, '2019-06-02 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -207,9 +209,8 @@ CREATE TABLE `pacienti` (
 --
 
 INSERT INTO `pacienti` (`ID`, `prenume`, `nume`, `email`, `data_nastere`, `telefon`, `sex`, `rh`, `grupa_sanguina`, `caz`, `public`, `data_cererii`, `activ`) VALUES
-(4, 'Dragan', 'Diana-Andreea', 'diana@gmail.com', '1996-08-20', 740151940, 'f', '+', 'AB', 'adfzsgxdhcvb', 'Da', '2019-03-16 16:40:06', 1),
 (6, 'Dragan', 'Diana-Andreea', 'dyusk_96@yahoo.com', '1996-08-20', 740151940, 'f', '-', 'A', 'werfgh', 'Nu', '2019-03-16 17:22:12', 1),
-(12, 'Diana-Andreea', 'Dragan', 'diana@gmail.com', '1996-08-20', 760373781, 'f', '+', 'AB', 'Nevoie urgenta de sange....', 'Da', '2019-04-03 10:48:16', 0);
+(12, 'Diana-Andreea', 'Dragan', 'dyusk_96@yahoo.com', '1996-08-20', 760373781, 'f', '+', 'AB', 'Nevoie urgenta de sange....', 'Da', '2019-04-03 10:48:16', 0);
 
 -- --------------------------------------------------------
 
@@ -228,7 +229,7 @@ CREATE TABLE `personal` (
 --
 
 INSERT INTO `personal` (`id`, `email`, `parola`) VALUES
-(1, 'bloodbank@gmail.com', '3a23bb515e06d0e944ff916e79a7775c');
+(1, 'centrulbloodbank@gmail.com', '3a23bb515e06d0e944ff916e79a7775c');
 
 -- --------------------------------------------------------
 
@@ -254,7 +255,8 @@ CREATE TABLE `spitale` (
 --
 
 INSERT INTO `spitale` (`ID`, `denumire`, `email`, `telefon`, `cantitate`, `rh`, `grupa`, `cazul`, `data_cererii`, `activ`) VALUES
-(4, 'Spitalul Judetean Slatina', 'sjSlatina@yahoo.com', 770506027, 10, '+', 'AB', 'sputal', '2019-04-03 07:52:38', 0);
+(4, 'Spitalul Judetean Slatina', 'sjSlatina@yahoo.com', 770506027, 10, '+', 'AB', 'sputal', '2019-04-03 07:52:38', 0),
+(5, 'Spitalul Judetean', 'spitalul_judetean@yahoo.com', 349000000, 10, '+', 'B', 'Lipsa stocului.', '2019-06-08 12:43:25', 1);
 
 -- --------------------------------------------------------
 
@@ -318,10 +320,10 @@ INSERT INTO `stoc` (`ID`, `grupa`, `rh`, `cantitate`) VALUES
 (2, '0', '-', 15),
 (3, 'A', '+', 15),
 (4, 'A', '-', 5),
-(5, 'B', '+', 37),
+(5, 'B', '+', 40),
 (6, 'B', '-', 15),
-(7, 'AB', '+', 9),
-(8, 'AB', '-', 3);
+(7, 'AB', '+', 17),
+(8, 'AB', '-', 8);
 
 -- --------------------------------------------------------
 
@@ -347,7 +349,8 @@ CREATE TABLE `voluntari` (
 INSERT INTO `voluntari` (`ID`, `nume`, `prenume`, `email`, `tip`, `mesaj`, `telefon`, `data_cererii`) VALUES
 (3, 'Dragan', 'Diana', 'diana@gmail.com', 'initiator', 'campanie de donare', 760373781, '2019-04-03 11:00:43'),
 (4, 'Dragan', 'Diana', 'dyusk_96@yahoo.com', 'voluntar', '', 760373781, '2019-05-09 17:36:21'),
-(5, 'Dragan', 'Diana', 'dyusk_96@yahoo.com', 'initiator', 'rytykh', 760373781, '2019-05-09 17:36:41');
+(5, 'Dragan', 'Diana', 'dragan_diana96@yahoo.com', 'voluntar', 'rytykh', 760373781, '2019-05-09 17:36:41'),
+(6, 'Dragan', 'Diana', 'dyusk_96@yahoo.com', 'voluntar', 'cewd', 760373781, '2019-06-11 20:54:03');
 
 --
 -- Indexuri pentru tabele eliminate
@@ -439,13 +442,13 @@ ALTER TABLE `cazuri_publicate`
 -- AUTO_INCREMENT pentru tabele `donatori`
 --
 ALTER TABLE `donatori`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT pentru tabele `mesaje`
 --
 ALTER TABLE `mesaje`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pentru tabele `pacienti`
@@ -463,7 +466,7 @@ ALTER TABLE `personal`
 -- AUTO_INCREMENT pentru tabele `spitale`
 --
 ALTER TABLE `spitale`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pentru tabele `stoc`
@@ -475,7 +478,7 @@ ALTER TABLE `stoc`
 -- AUTO_INCREMENT pentru tabele `voluntari`
 --
 ALTER TABLE `voluntari`
-  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ bloodbank/a_centru/application/views/email/response.php
@@ -0,0 +1,119 @@
+
+	<div class="breadcomb-area">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+					<div class="breadcomb-list">
+						<div class="row">
+							<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+								<div class="breadcomb-wp">
+									<div class="breadcomb-icon">
+										<i class="fa fa-edit"></i>
+									</div>
+									<div class="breadcomb-ctn">
+										<h2>Compunere email</h2>
+										<p>centrulBloodBank@gmail.com</p>
+									</div>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Breadcomb area End-->
+    <!-- Compose email area Start-->
+    <div class="inbox-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+                    <div class="inbox-left-sd">
+                        <div class="compose-ml">
+                            <a class="btn" href="#">Compunere</a>
+                        </div>
+                        <div class="inbox-status">
+                            <ul class="inbox-st-nav inbox-ft">
+                                <li><a href="<?php echo base_url('inbox')?>"><i class="fa fa-envelope"></i> Mesaje primite</a></li>
+                                <li><a href="<?php echo base_url('send')?>"><i class="fa fa-paper-plane"></i> Mesaje trimise</a></li>
+                            
+                            </ul>
+                        </div>
+                        <hr>
+                      
+                    </div>
+                </div>
+                <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12">
+                    
+                    <div class="view-mail-list sm-res-mg-t-30">
+                        <div class="view-mail-hd">
+                            <div class="view-mail-hrd">
+                                <h2>Mesaj nou</h2>
+                            </div>
+                        </div>
+                        <form method="post" action="<?php echo base_url('email/sendEmail')?>"> 
+                        <div class="cmp-int mg-t-20">
+                            <div class="row">
+                                <div class="col-lg-1 col-md-2 col-sm-2 col-xs-12">
+                                    <div class="cmp-int-lb cmp-int-lb1 text-right">
+                                        <span>To: </span>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-10 col-sm-10 col-xs-12">
+                                    <div class="form-group">
+                                        <div class="nk-int-st cmp-int-in cmp-email-over">
+                                            <input type="email" name="email" class="form-control" value="<?php echo $mesaj[0]->email?>"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                         <!--    <div class="row">
+                                <div class="col-lg-1 col-md-2 col-sm-2 col-xs-12">
+                                    <div class="cmp-int-lb cmp-int-lb1 text-right">
+                                        <span>Cc: </span>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-10 col-sm-10 col-xs-12">
+                                    <div class="form-group">
+                                        <div class="nk-int-st cmp-int-in cmp-email-over">
+                                            <input type="email" class="form-control" placeholder="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> -->
+                            <div class="row">
+                                <div class="col-lg-1 col-md-2 col-sm-2 col-xs-12">
+                                    <div class="cmp-int-lb text-right">
+                                        <span>Subiect: </span>
+                                    </div>
+                                </div>
+                                <div class="col-lg-11 col-md-10 col-sm-10 col-xs-12">
+                                    <div class="form-group cmp-em-mg">
+                                        <div class="nk-int-st cmp-int-in cmp-email-over">
+                                            <input type="text" name="subiect" class="form-control" placeholder="" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="cmp-int-box mg-t-20">
+                            <div class="html-editor-cm">
+                                <textarea rows="10" cols="113" type="text" name="mesaj">
+                                </textarea>
+                            </div>
+                        </div>
+
+                        <div class="compose-ml col-md-3" style="padding-top: 30px;">
+                            <input class="btn" style="background: #8B0000;" type="submit" name="submit" value="Trimite">
+                        </div>
+                    </form>
+                        
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- Compose email area End
